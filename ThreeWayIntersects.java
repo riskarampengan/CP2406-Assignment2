4af4206d53c8f2bca127f2935f1e57f64392de5f
riskarampengan <53027261+riskarampengan@users.noreply.github.com>
2020-01-24 19:06:40 +0800

    Add files via upload

---
+++ ThreeWayIntersects.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class ThreeWayIntersects extends Road {
+    public ThreeWayIntersects(int road_id, int next_road_id, int x, int y, int road_width, int road_height, Color color) {
+        super(road_id, next_road_id, x, y, road_width, road_height, color);
+    }
+}
